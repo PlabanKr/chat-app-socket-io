e698250308fa11737f80d7f19fe50f1d47389160
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-01-04 00:59:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// TODO: environment variable
+// TODO: filter foul words
+// TODO: add date time with message
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`listening to the port:${PORT}`);
+});
